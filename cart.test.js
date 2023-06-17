247a34e01c3ae53c3b9b3e669593ea82d9f5efac
Suraj Singh <surajsingh220698@outlook.com>
2023-06-17 17:58:49 +0530

    Use assert library to compare the items.

--- cart.test.js
+++ cart.test.js
@@ -1,19 +1,9 @@
 const Cart = require('./Cart.js');
+const assert = require('assert');
 
 const cart = new Cart();
-cart.addToCart('Cupcake');
+cart.addToCart('Cheesecake');
 
-const hasAnItem = cart.items.length === 1; 
-const hasACheeseCake = cart.items[0] === 'Cheesecake';
+assert.deepStrictEqual(cart.items, ['Cheesecake']);
 
-if (hasAnItem && hasACheeseCake) {
-  console.log('The addToCart function can add an item to the cart.');
-}
-else {
-  const items = cart.items.join(', ');
-
-  console.error('The add to cart function did not do what we expect.');
-  console.error(`Here are the actual content of the cart: ${items}`);
-
-  throw new Error("The Test case failed");
-}
+console.log('The addToCart function can add an item to the cart.');
