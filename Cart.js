8f81e8b417f6285038009b29edd74798a0af0109
Suraj Singh <surajsingh220698@outlook.com>
2023-06-18 12:30:43 +0530

    Setup jest and add remove an item helper function with an additional test case.

--- Cart.js
+++ Cart.js
@@ -6,6 +6,13 @@ class Cart {
   addToCart(item) {
     this.items.push(item);
   }
+
+  removeAnItemFromCart(item) {
+    const index = this.items.indexOf(item);
+    if(index > -1) {
+      this.items.splice(index, 1);
+    }
+  }
 }
 
 module.exports = Cart;
--- cart.test.js
+++ cart.test.js
@@ -1,9 +1,25 @@
 const Cart = require('./Cart.js');
-const assert = require('assert');
 
-const cart = new Cart();
-cart.addToCart('Cheesecake');
+//  TEST HELPS US TO ORGANISE MULTIPLE TESTS WITHIN A SINGLE FILE AND INDICATES WHAT SHOULD RUN
+test("The addToCart function can add an item to the cart.", () => {
+  //  ARRANGE
+  const cart = new Cart();
 
-assert.deepStrictEqual(cart.items, ['Cheesecake']);
+  //  ACTS
+  cart.addToCart('Cheesecake');
 
-console.log('The addToCart function can add an item to the cart.');
+  //  ASSERTS
+  expect(cart.items).toEqual(['Cheesecake']);
+});
+
+test("The removeAnItemFromCart function can remove an item to the cart.", () => {
+  //  ARRANGE
+  const cart = new Cart();
+  cart.addToCart('Cheesecake');
+
+  //  ACTS
+  cart.removeAnItemFromCart('Cheesecake');
+
+   //  ASSERTS
+  expect(cart.items).toEqual([]);
+});
