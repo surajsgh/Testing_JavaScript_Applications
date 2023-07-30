19513bed6618caa161e3207941e83671e6ddd104
Suraj Singh <surajsingh220698@outlook.com>
2023-07-30 14:45:19 +0530

    Add a prior / before every routes.

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = new Koa();
 const router = new Router();
 const carts = new Map();
 
-router.get('carts/:username/items', ctx => {
+router.get('/carts/:username/items', ctx => {
   const cart = carts.get(ctx.params.username);
   cart ? (ctx.body = cart) : (ctx.status = 404);
 });
 
-router.post('carts/:username/items/:item', ctx => {
+router.post('/carts/:username/items/:item', ctx => {
   const { username, item } = ctx.params;
   const newItems = (carts.get(username) || []).concat(item);
   carts.set(username, newItems);
