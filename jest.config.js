d206ec6bfc3d3ee3cb6b9b39d63d6e28ddf28045
surajsgh <surajsingh220698@outlook.com>
2023-11-12 13:18:27 +0530

    Comment the global hooks.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: "node",
-  globalSetup: "./globalSetup.js",
-  globalTeardown: "./globalTeardown.js"
+  // testEnvironment: "node",
+  // globalSetup: "./globalSetup.js",
+  // globalTeardown: "./globalTeardown.js"
 };
