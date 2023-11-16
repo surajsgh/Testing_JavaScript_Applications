f7c2c0d378c30967fb54a4ef56d6d525ded8092b
surajsgh <surajsingh220698@outlook.com>
2023-11-16 23:18:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const { getInventory } = require('./inventoryController');
+// const { getInventory } = require('./inventoryController');
 
 const app = new Koa();
 const router = new Router();
@@ -14,25 +14,37 @@ router.get('/carts/:username/items', ctx => {
 
 router.post('/carts/:username/items/:item', ctx => {
   const { username, item } = ctx.params;
-  if(!inventory.has(item)) {
+  const isAvailable = inventory.has(item) && inventory.get(item) > 0;
+  if(!isAvailable) {
+    ctx.body = { message: `${item} is unavailable.` };
     ctx.status = 404;
     return;
   }
-  inventory.set(item, inventory.get(item) - 1);
   const newItems = (carts.get(username) || []).concat(item);
+  carts.set(username, newItems);
+  inventory.set(item, inventory.get(item) - 1);
+  ctx.body = newItems;
+});
+
+router.delete('/carts/:username/items/:item', ctx => {
+  const { username, item } = ctx.params;
+  if(!carts.has(username) || !carts.get(username).includes(item)) {
+    ctx.body = `${item} is not in the cart.`;
+    ctx.status = 400;
+    return;
+  }
+
+  const newItems = (carts.get(username) || []).filter((cartItem => cartItem !== item));
+  inventory.set(item, (inventory.get(item) || 0) + 1);
   carts.set(username, newItems);
   ctx.body = newItems;
 });
 
-router.get('/inventory', ctx => {
-  console.log('Test');
-  ctx.body = getInventory()
-});
+// router.get('/inventory', ctx => {
+//   console.log('Test');
+//   ctx.body = getInventory()
+// });
 
 app.use(router.routes());
 
-module.exports = {
-  app: app.listen(3000),
-  inventory,
-  carts
-};
+module.exports = app.listen(3000);
